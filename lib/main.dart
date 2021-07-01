bbf70fc6f8ad384ea84c85b3b2f9158c991cd518
Joseph Shaaban <joseph.sha3ban@gmail.com>
2021-07-01 11:44:51 +0300

    fix notification navigator

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 //import 'item3.dart';
+import 'package:flutter/foundation.dart';
+import 'package:hello_world1/item3.dart';
+
 import 'splashScreen.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -26,6 +29,62 @@ Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
       ));
 }
 
+
+class MessageHandler extends StatefulWidget {
+  final Widget child;
+  MessageHandler({this.child});
+  @override
+  State createState() => MessageHandlerState();
+}
+
+class MessageHandlerState extends State<MessageHandler> {
+  Widget child;
+  @override
+  void initState() {
+    super.initState();
+    child=this.widget.child;
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage message) {
+      if (message != null) {
+        Navigator.push(context, MaterialPageRoute(builder: (context)=>Item3Screen(payload: message.notification.body,)));
+      }
+    });
+
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      RemoteNotification notification = message.notification;
+      AndroidNotification android = message.notification?.android;
+      if (notification != null && android != null && !kIsWeb) {
+        flutterLocalNotificationsPlugin.show(
+            notification.hashCode,
+            notification.title,
+            notification.body,
+            NotificationDetails(
+              android: AndroidNotificationDetails(
+                channel.id,
+                channel.name,
+                channel.description,
+                // TODO add a proper drawable resource to android, for now using
+                //      one that already exists in example app.
+                icon: 'launch_background',
+              ),
+            ));
+      }
+    });
+
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('A new onMessageOpenedApp event was published!');
+      Navigator.push(context, MaterialPageRoute(builder: (context)=>Item3Screen(payload: message.notification.body,)));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return child;
+  }
+}
+
+
 const AndroidNotificationChannel channel = AndroidNotificationChannel(
   'high_importance_channel',
   'high_importance_notification',
@@ -40,7 +99,10 @@ final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+
+
   FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+
 
   await flutterLocalNotificationsPlugin
   .resolvePlatformSpecificImplementation<AndroidFlutterLocalNotificationsPlugin>()
@@ -50,10 +112,13 @@ void main() async {
   var email = preferences.getString('email');
     runApp(
         MaterialApp(
+          routes: {
+            '/notification':(context) => Item3Screen()
+          },
             debugShowCheckedModeBanner: true,
-            home: email == null ? SplashScreen() : email == 'admin@gmail.com'
+            home: MessageHandler(child:email == null ? SplashScreen() : email == 'admin@gmail.com'
                 ? AdminScreen()
-                : NewsScreen()));
+                : NewsScreen())));
   }
 
 class AppTheme {
